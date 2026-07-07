566c0941a2cd5e4f3e6003f81a6fedff693ce518
3fuuuu <fuuuu1009@icloud.com>
2026-07-07 15:45:49 +0900

    1

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`${client.user?.tag} is Ready!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    await message.reply("Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
